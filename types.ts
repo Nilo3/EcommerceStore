be61b00ba4eae722fda2415ba8bc09f943470b3c
Nilo Echezarreta <nilo.echezarreta@gmail.com>
2023-07-30 15:18:30 -0300

    Cart view and summary complete

--- types.ts
+++ types.ts
@@ -9,16 +9,16 @@ export interface Category {
     name: string;
     billboard: Billboard;
 }
-
 export interface Product {
     id: string;
     category: Category;
     name: string;
+    price: string;
     isFeatured: boolean;
     size: Size;
     fragrance: Fragrance;
     images: Image[]
-}
+  };
 
 export interface Image {
     id: string;
